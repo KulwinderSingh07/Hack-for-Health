500f9871f5d2cd664f8a8ffcfd93fa5a55fcd4f5
Kulwinder <laddiaulakh2002@gmail.com>
2024-01-10 22:35:50 +0530

    initial commit

---
+++ client/src/components/mainDashBoard.js
@@ -0,0 +1,12 @@
+const MainDashBoardComponent = () => {
+    return ( 
+        <div className="DashBoardWrapper">
+            <div className="DashBoardHeader">
+                <h3>DashBoard</h3>
+                <div>ProfilePic</div>
+            </div>
+        </div>
+     );
+}
+ 
+export default MainDashBoardComponent;
