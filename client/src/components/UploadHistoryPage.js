e2e6fab22b2c36d1a820834c88c9cadf2d6f48d5
Kulwinder <laddiaulakh2002@gmail.com>
2024-01-15 01:20:02 +0530

    added pdf history List

--- client/src/components/UploadHistoryPage.js
+++ client/src/components/UploadHistoryPage.js
@@ -2,20 +2,127 @@ import React from 'react'
 
 //MATERIAL UI IMPORTS
 import PictureAsPdfIcon from '@mui/icons-material/PictureAsPdf';
+import ListSubheader from '@mui/material/ListSubheader';
+import List from '@mui/material/List';
+import ListItemButton from '@mui/material/ListItemButton';
+import { IconButton } from '@mui/material';
+import FileDownloadIcon from '@mui/icons-material/FileDownload';
+import OpenInFullIcon from '@mui/icons-material/OpenInFull';
+import { useState } from 'react';
+import { Typography } from "@mui/material";
 
 //CSS
 import '../CSS/UploadHistoryPage.css';
+import HistoryPopUpCompoent from './popUpForHistory';
 
 
 const UploadHistoryPage = () => {
+  const [openPopUp, setOpenPopUp] = useState(-1)
+  const data="hello ji"
+
+  const ListData=[
+    {
+        val:"Inbox",
+        date:new Date().toDateString(),
+        output:"Ill"
+      },
+      {
+        val:"Number",
+        date:new Date().toDateString(),
+        output:"Ill"
+      },
+      {
+        val:"Window",
+        date:new Date().toDateString(),
+        output:"Ill"
+      },
+      {
+        val:"HEllo",
+        date:new Date().toDateString(),
+        output:"Ill"
+      },
+      {
+        val:"HEllo",
+        date:new Date().toDateString(),
+        output:"Ill"
+      },
+      {
+        val:"HEllo",
+        date:new Date().toDateString(),
+        output:"Ill"
+      },
+      {
+        val:"HEllo",
+        date:new Date().toDateString(),
+        output:"Ill"
+      }
+  ]
+
+  const handleClick = (index) => {
+    console.log(index)
+    if(index==openPopUp){
+      setOpenPopUp(-1)
+    }else{
+      setOpenPopUp(index);
+    }
+  };
+
+
   return (
     <>
     <div className='uploadHistoryPageMainDiv'>
-
         {/*Imported Header*/}
         <div className='exercisesSuggestionHeader'>
             <PictureAsPdfIcon sx={{fontSize:'2vw',marginRight:'15px',marginTop:'-0.2vw'}}/><h3 className='exercisesHeaderText'>FILE UPLOAD HISTORY</h3>
         </div>
+
+        <div className='reportHistoryHeading'>
+          <div>Download</div>
+          <div>PDF Name</div>
+          <div>Date/Time</div>
+          {/* <div>Download Button</div> */}
+          <div>Report Output</div>
+        </div>
+
+        <div className='ReportHistoryDrop'>
+    <List
+    className='listWrapper'
+      component="nav"
+      aria-labelledby="nested-list-subheader"
+    >
+        {ListData.map((ele,index)=>{
+            return(
+                <div>
+      <ListItemButton
+      className='listItem'
+      sx={{marginBottom:"1vh", borderRadius:"1rem",background:"CE16EC",background:"linear-gradient(225deg, #CE16EC, #4F7EF2)",":hover":{backgroundColor:"#4156ca"}}}
+      >
+        <div className='listItemWrapper'>
+          <div className='listItemObject'>
+        <IconButton className='iconForDownload'>
+          <FileDownloadIcon style={{color:"white"}} fontSize='medium'/>
+        </IconButton>
+          </div>
+        <Typography  className='listItemObject' sx={{fontSize:"large",fontWeight:"bold"}}>{ele.val}</Typography>
+        <Typography className='listItemObject'  sx={{fontSize:"large",fontWeight:"bold"}}>{ele.date}</Typography>
+        <div className='popUpOutput listItemObject'>
+          <Typography  sx={{fontSize:"large",fontWeight:"bold" }}>{ele.output}</Typography>      
+        <IconButton
+        onClick={()=>{
+          handleClick(index)
+        }}
+        >
+          <OpenInFullIcon fontSize='medium'/>
+        </IconButton>
+        </div>
+        </div>
+      </ListItemButton>
+      <HistoryPopUpCompoent data={data} open={openPopUp} index={index} setOpenPopUp={setOpenPopUp}/>
+                </div>
+            )
+        })}
+    </List>
+  </div>
     
     </div>
     </>
---
+++ client/src/components/popUpForHistory.js
@@ -0,0 +1,72 @@
+import Button from '@mui/material/Button';
+import { Fragment } from 'react';
+import { styled } from '@mui/material/styles';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogActions from '@mui/material/DialogActions';
+import IconButton from '@mui/material/IconButton';
+import CloseIcon from '@mui/icons-material/Close';
+import Typography from '@mui/material/Typography';
+import { useState } from 'react';
+
+const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+  '& .MuiDialogContent-root': {
+    padding: theme.spacing(4)
+  },
+  '& .MuiDialogActions-root': {
+    padding: theme.spacing(1),
+  },
+}));
+
+const HistoryPopUpCompoent = ({open,setOpenPopUp,data,index}) => {
+    const handleClose = () => {
+        setOpenPopUp(-1);
+    };
+    return ( 
+        <Fragment>
+          <Dialog onClose={handleClose}
+            open={open==index?true:false}
+            sx={{backdropFilter:"blur(5px) sepia(5%)"}}
+            PaperProps={{sx:{borderRadius:"1rem"}}}
+          >
+        {/* <BootstrapDialog
+          onClose={handleClose}
+          aria-labelledby="customized-dialog-title"
+          > */}
+          <DialogTitle sx={{ m: 0, p: 2 }} id="customized-dialog-title">
+            Modal title
+          </DialogTitle>
+          <IconButton
+            aria-label="close"
+            onClick={handleClose}
+            sx={{
+              position: 'absolute',
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+            >
+            <CloseIcon />
+          </IconButton>
+          <DialogContent dividers>
+            <Typography gutterBottom>
+             {data}
+            </Typography>
+            <Typography gutterBottom>
+              Praesent commodo cursus magna, vel scelerisque nisl consectetur et.
+              Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor.
+            </Typography>
+            <Typography gutterBottom>
+              Aenean lacinia bibendum nulla sed consectetur. Praesent commodo cursus
+              magna, vel scelerisque nisl consectetur et. Donec sed odio dui. Donec
+              ullamcorper nulla non metus auctor fringilla.
+            </Typography>
+          </DialogContent>
+        {/* </BootstrapDialog> */}
+            </Dialog>
+      </Fragment>
+    );
+}
+ 
+export default HistoryPopUpCompoent;
