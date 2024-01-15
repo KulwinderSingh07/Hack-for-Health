8f18f55fc7472cb712de2fe57da65caee08e4dc6
SimardeepSingh1450 <smudhar_be21@thapar.edu>
2024-01-15 22:03:57 +0530

    update: calendar api integration done

---
+++ server/models/calendarModel.js
@@ -0,0 +1,25 @@
+const mongoose=require("mongoose")
+const calendarSchema=mongoose.Schema({
+    username:{
+        type:String
+    },
+    email:{
+        type:String
+    },
+    dates:{
+        type: [
+            // Define the type of elements inside the dates array
+            {
+                type: [mongoose.Schema.Types.Mixed] // Array of integers
+            }
+        ]
+    },
+    time:{
+        type:String
+    }
+})
+
+//dates -> [day,month,year] -> all values in integer
+
+const calendarModel=mongoose.model("calendarModel",calendarSchema)
+module.exports=calendarModel
